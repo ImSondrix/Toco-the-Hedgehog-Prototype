b551df875ef905298443cc3d1061936e636bd79d
Sondrix <sh4740560@gmail.com>
2025-05-22 17:08:20 +0000

    Pending changes exported from your codespace

---
+++ scripts/project/javaScriptInEvents.js
@@ -0,0 +1,21 @@
+
+
+const scriptsInEvents = {
+
+	async ["Menu&OptionsScreen_Event1"](runtime, localVars)
+	{
+		
+	},
+
+	async LayoutScreen_Event1(runtime, localVars)
+	{
+		
+	},
+
+	async RobotBoss_Event56(runtime, localVars)
+	{
+
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
